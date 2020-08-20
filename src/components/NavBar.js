04d9824f9984ae656e48d4f87b7f4bcf513a44b1
taofik-adeniyi <bidemi64@gmail.com>
2020-08-20 15:39:20 +0100

    linked the images and logos that would take you home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './NavBar.css'
 import logo from '../images/yellostarlogo.svg'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
     return (
         <div className="NavBar">
-            
-            <img src={logo} alt="YelloStarLogo" className="Image"  />
-            
+            <Link to="/">
+                <img src={logo} alt="YelloStarLogo" className="Image"  />
+            </Link>
             <div className="List">
                 <ul>
                     <li>How to Play</li>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {FaHome} from 'react-icons/fa'
 import mtnLogo from '../images/mtn-logo.svg'
+import { Link } from 'react-router-dom'
 
 function SideBar(props) {
     return (
         <div className="sidebar">
-            <div className="nav-items">             
-                <img src={mtnLogo} alt="mtn-logo" className="mtn-logo" />
-                
+            <div className="nav-items">
+                <Link to="/">             
+                    <img src={mtnLogo} alt="mtn-logo" className="mtn-logo" />
+                </Link>
                 <div onClick={props.homeControl} className="items cor">
                     <div><FaHome color="white" size="25" /></div>
                     <div>Home</div>
